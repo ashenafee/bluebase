68efc0e7483b06f6173a9e8d2501db82765f2f15
erica27 <erica.eng@mail.utoronto.ca>
2022-02-05 23:40:28 -0500

    Embed so far

--- weather_embed.py
+++ weather_embed.py
@@ -19,22 +19,21 @@ async def test(ctx):
 
 @client.command()
 async def weather(ctx):
-    await ctx.send('Today the weather is')
+
     today_date = date.today()
     weather_stats = w_data.get_weather_data(today_date)
     embed = discord.Embed(
         title='The Weather for ' + str(today_date),
-        description=weather_stats,
+        description='Today is' + str(weather_stats[0]),
         colour=discord.colour.Colour.blue()
     )
 
-    embed.set_footer(text='this is the footer')
     embed.set_image(url='https://media.globalnews.ca/videostatic/621/967/2016-03-08T17-14-44.133Z--1280x720.jpg')
     embed.set_thumbnail(url='https://media.globalnews.ca/videostatic/621/967/2016-03-08T17-14-44.133Z--1280x720.jpg')
     embed.set_author(name='Bluebot')
     embed.add_field(name='Weather', value='Here is your forecast', inline=True)
-    # await ctx.send(embed=embed)
 
+    await ctx.send(embed=embed)
 
 
 client.run(TOKEN)
