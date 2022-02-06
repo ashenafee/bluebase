0f5373cb7522bc546f23cd76f2066c870282003b
erica27 <erica.eng@mail.utoronto.ca>
2022-02-05 23:25:20 -0500

    Initial !test command

--- weather_embed.py
+++ weather_embed.py
@@ -3,8 +3,8 @@ from discord.ext import commands
 import parse_weather_data as w_data
 from datetime import date
 
-TOKEN = 'YOUR_TOKEN_HERE'
-client = commands.Bot(command_prefix='+')
+TOKEN = 'TOKEN_HERE'
+client = commands.Bot(command_prefix='!')
 
 
 @client.event
@@ -12,8 +12,14 @@ async def on_ready():
     print('WeatherBot is ready to run')
 
 
-@client.command(pass_context=True)
-async def display(ctx):
+@client.command()
+async def test(ctx):
+    await ctx.send('Ask me what the weather is!')
+
+
+@client.command()
+async def weather(ctx):
+    await ctx.send('Today the weather is')
     today_date = date.today()
     weather_stats = w_data.get_weather_data(today_date)
     embed = discord.Embed(
@@ -27,8 +33,9 @@ async def display(ctx):
     embed.set_thumbnail(url='https://media.globalnews.ca/videostatic/621/967/2016-03-08T17-14-44.133Z--1280x720.jpg')
     embed.set_author(name='Bluebot')
     embed.add_field(name='Weather', value='Here is your forecast', inline=True)
+    # await ctx.send(embed=embed)
 
-    await ctx.run(embed=embed)
+
 
 client.run(TOKEN)
 
